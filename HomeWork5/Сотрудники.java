6d67f6dab44a5b7897de39bba95a147a8fd9147f
nata <nata@yalanskaya>
2021-08-02 22:34:01 +0300

    Домашняя работа N5 Яланская Н.

---
+++ HomeWork5/Сотрудники.java
@@ -0,0 +1,29 @@
+package HomeWork5;
+
+ class Сотрудники {
+     String ФИО;
+     String Должность;
+     String email;
+     int телефон;
+     int зарплата;
+     int возраст;
+
+
+     public Сотрудники(String ФИО, String Должность, String email, int телефон, int зарплата, int возраст) {
+         this.ФИО = ФИО;
+         this.Должность = Должность;
+         this.email = email;
+         this.зарплата = зарплата;
+         this.возраст = возраст;
+         this.телефон = телефон;
+
+     }
+ }
+
+
+
+
+
+
+
+
