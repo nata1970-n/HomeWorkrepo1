d4bdd303d8fe43a28c49dff202903d524b3dd77c
nata <nata@yalanskaya>
2021-08-07 20:00:49 +0300

    Домашняя работа N 6 Яланская Н.

---
+++ src/HomeWork6/Dog.java
@@ -0,0 +1,10 @@
+package HomeWork6;
+
+     class Dog extends Animals{
+     Dog(String type,String name,int maxLengthRun,int maxLengthSwim) {
+        super(type,name,maxLengthRun,maxLengthSwim);
+
+    }
+
+    }
+
